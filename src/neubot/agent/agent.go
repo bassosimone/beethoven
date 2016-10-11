204469147d25be7a0d9567b2c12688a1c4313ff7
Simone Basso <bassosimone@gmail.com>
2016-10-11 18:11:06 +0200

    Start sketching out API using martini

--- src/neubot/agent/agent.go
+++ src/neubot/agent/agent.go
@@ -1,4 +1,15 @@
 package agent
 
+import (
+	"fmt"
+	"github.com/go-martini/martini"
+)
+
 func Run(address string, port int, verbose bool) {
+	m := martini.Classic()
+	m.Get("/", func() string {
+		return "Hello world!"
+	})
+	endpoint := fmt.Sprintf("%s:%d", address, port)
+	m.RunOnAddr(endpoint)
 }
