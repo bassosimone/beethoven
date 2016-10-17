8148313bc1c4efa37b92757e1866e6cf30faa7f4
Simone Basso <bassosimone@gmail.com>
2016-10-17 14:09:25 +0200

    Simplify version

--- common/version.go
+++ common/version.go
@@ -1,6 +1,4 @@
 package common
 
-const Version = "0.0.1"
-const BeethovenVersion = Version
-const NeubotVersion = "0.5.0"
-const Product = "beethoven/" + BeethovenVersion + " neubot/" + NeubotVersion
+const Version = "0.5.0"
+const Product = "neubot/" + Version
